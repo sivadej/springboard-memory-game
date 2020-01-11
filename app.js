b344ba4355ae40f853054ccc07117f52cb10a0cd
Sivadej K <sbkitchpanich@gmail.com>
2020-01-11 07:06:23 -0900

    actually first commit

---
+++ app.js
@@ -0,0 +1,127 @@
+// Sivadej Kitchpanich
+
+const counter = document.querySelector('#counter'); //total card "flips" per game
+const newGameBtn = document.querySelector('#new-game-btn');
+const gameBoard = document.querySelector('#gameboard');
+let shuffledCards = shuffle([ '01', '01', '02', '02', '03', '03', '04', '04' ]); //to set image src and IDs for matching pairs
+let bestScore = localStorage.getItem('best-score');
+let movesCounted = 0;
+let cardsInPlay = 0;
+let firstCardPicked = '';
+let firstPickSlot = '';
+let secondCardPicked = '';
+let secondPickSlot = '';
+let matchesFound = 0;
+
+document.querySelector('#best-score').innerHTML = bestScore;
+
+displayCards();
+
+newGameBtn.addEventListener('click', resetGame);
+
+gameBoard.addEventListener('click', function(e) {
+	if (e.target.tagName === 'IMG') {
+		e.target.classList.add('disabled');
+		cardsInPlay++;
+		increaseCounter();
+		//console.log('cards in play: ' + cardsInPlay);
+		if (cardsInPlay === 1) {
+			e.target.setAttribute('src', `images/${e.target.dataset.id}.jpg`);
+			firstCardPicked = e.target.dataset.id;
+			firstPickSlot = e.target.id;
+		}
+		else if (cardsInPlay === 2) {
+			e.target.setAttribute('src', `images/${e.target.dataset.id}.jpg`);
+			secondCardPicked = e.target.dataset.id;
+			secondPickSlot = e.target.id;
+			//console.log(`Checking for match... ids picked: ${firstCardPicked} and ${secondCardPicked}`);
+			//console.log(`Slots picked: ${firstPickSlot} and ${secondPickSlot}`);
+			cardsInPlay = 0;
+			if (firstCardPicked === secondCardPicked) {
+				//console.log('woohoo, its a match!');
+				matchesFound++;
+				if (matchesFound === 4) {
+					document.getElementById('status').innerHTML = '<b>WINNER!</b>';
+					updateBestScore(movesCounted);
+				}
+			}
+			else resetMismatch(firstPickSlot, secondPickSlot);
+		}
+	}
+});
+
+function displayCards() {
+	gameBoard.innerHTML = '';
+	for (let [ index, card ] of shuffledCards.entries()) {
+		//index represents each slot on the board (slot0-slot7)
+		const newCard = document.createElement('img');
+		//newCard.setAttribute('src', `images/${card}.jpg`);
+		newCard.setAttribute('id', 'slot' + index);
+		newCard.setAttribute('data-id', card);
+		newCard.setAttribute('class', 'card');
+		gameBoard.append(newCard);
+	}
+	let allCards = gameBoard.querySelectorAll('.card');
+	for (let card of allCards) {
+		card.setAttribute('src', 'images/hidden.jpg');
+	}
+}
+
+function resetGame() {
+	//reset all counts, reshuffle card array, hide cards
+	gameBoard.classList.remove('disabled');
+	const gameImgs = gameBoard.querySelectorAll('img');
+	for (const img of gameImgs) img.classList.remove('disabled');
+
+	movesCounted = 0;
+	counter.innerHTML = '0';
+	document.getElementById('status').innerHTML = '';
+	shuffledCards = shuffle([ '01', '01', '02', '02', '03', '03', '04', '04' ]);
+	cardsInPlay = 0;
+	firstCardPicked = '';
+	firstPickSlot = '';
+	secondCardPicked = '';
+	secondPickSlot = '';
+	matchesFound = 0;
+	displayCards();
+}
+
+function updateBestScore(score){
+	if (score < bestScore){
+		localStorage.setItem('best-score',score);
+		document.querySelector('#best-score').innerHTML = 'NEW! ' + score;
+	}
+}
+
+function increaseCounter() {
+	movesCounted++;
+	counter.innerHTML = movesCounted;
+}
+
+// Fisher-Yates (aka Knuth) Shuffle
+function shuffle(array) {
+	var currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
+
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+
+function resetMismatch(firstSlot, secondSlot) {
+	gameBoard.classList.toggle('disabled'); //to prevent cards from being revealed while waiting to reset the mismatch
+	const reset = setTimeout(function() {
+		document.getElementById(firstSlot).setAttribute('src', 'images/hidden.jpg');
+		document.getElementById(firstSlot).classList.remove('disabled');
+		document.getElementById(secondSlot).setAttribute('src', 'images/hidden.jpg');
+		document.getElementById(secondSlot).classList.remove('disabled');
+		gameBoard.classList.toggle('disabled');
+		//console.log('cleared!');
+	}, 1000);
+}
